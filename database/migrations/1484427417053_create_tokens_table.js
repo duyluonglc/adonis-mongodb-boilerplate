2be1ab4eb16871cc2c6a4949d712b5611b93f003
Duy Lượng <duyluonglc@users.noreply.github.com>
2017-04-30 22:27:28 +0700

    Update 1484427417053_create_tokens_table.js

--- database/migrations/1484427417053_create_tokens_table.js
+++ database/migrations/1484427417053_create_tokens_table.js
@@ -5,13 +5,13 @@ const Schema = use('Schema')
 class TokensTableSchema extends Schema {
   up () {
     this.create('tokens', table => {
-      table.increments()
-      table.integer('user_id').unsigned().references('id').inTable('users')
-      table.string('token', 40).notNullable().unique()
-      table.boolean('forever').defaultTo(false)
-      table.boolean('is_revoked').defaultTo(false)
-      table.timestamp('expiry')
-      table.timestamps()
+      // table.increments()
+      // table.integer('user_id').unsigned().references('id').inTable('users')
+      // table.string('token', 40).notNullable().unique()
+      // table.boolean('forever').defaultTo(false)
+      // table.boolean('is_revoked').defaultTo(false)
+      // table.timestamp('expiry')
+      // table.timestamps()
     })
   }
 
