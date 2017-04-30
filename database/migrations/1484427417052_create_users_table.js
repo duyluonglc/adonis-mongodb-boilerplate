80dc10a63fb652ce7faddd58c75379b11a96784e
Duy Lượng <duyluonglc@users.noreply.github.com>
2017-04-30 22:26:48 +0700

    Update 1484427417052_create_users_table.js

--- database/migrations/1484427417052_create_users_table.js
+++ database/migrations/1484427417052_create_users_table.js
@@ -5,11 +5,11 @@ const Schema = use('Schema')
 class UsersTableSchema extends Schema {
   up () {
     this.create('users', table => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
+      // table.increments()
+      // table.string('username', 80).notNullable().unique()
+      // table.string('email', 254).notNullable().unique()
+      // table.string('password', 60).notNullable()
+      // table.timestamps()
     })
   }
 
