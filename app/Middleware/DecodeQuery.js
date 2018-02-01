1e236608b4e91195848ce8c2cf20d83566d5e104
Duy Luong <luongnd@phi.vn>
2018-02-01 23:22:31 +0700

    fix(decodeQuery): remove default limit

--- app/Middleware/DecodeQuery.js
+++ app/Middleware/DecodeQuery.js
@@ -1,6 +1,5 @@
 'use strict'
 const _ = require('lodash')
-const Config = use('Config')
 const { InvalidArgumentException } = require('@adonisjs/generic-exceptions')
 
 class DecodeQuery {
@@ -14,8 +13,6 @@ class DecodeQuery {
           throw InvalidArgumentException.invoke(error.message)
         }
       }
-      query.limit = query.limit || Config.get('api.limit', 20)
-      query.skip = query.skip || 0
       query.where = query.where || {}
       let includes = query.with || []
       if (_.isString(includes)) {
@@ -45,7 +42,7 @@ class DecodeQuery {
         throw InvalidArgumentException.invoke('`with` is not valid format')
       }
       query.with = includes
-      return _.pick(query, ['select', 'skip', 'limit', 'where', 'sort', 'limit', 'with'])
+      return _.pick(query, ['select', 'skip', 'limit', 'where', 'sort', 'with'])
     }
     // await next to pass the request to next middleware or controller
     await next()
