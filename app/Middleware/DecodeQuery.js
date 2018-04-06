c3aa8a2db3aa7663e3f36bba7c3a0a4ddc7cdc90
Duy Luong <luongnd@phi.vn>
2018-04-06 16:24:22 +0700

    fix decode query

--- app/Middleware/DecodeQuery.js
+++ app/Middleware/DecodeQuery.js
@@ -3,45 +3,19 @@ const _ = require('lodash')
 const { InvalidArgumentException } = require('@adonisjs/generic-exceptions')
 
 class DecodeQuery {
-  async handle (ctx, next, modelName) {
+  async handle (ctx, next) {
     ctx.decodeQuery = () => {
       let query = {}
-      if (this.input('query')) {
+      if (ctx.request.input('query')) {
         try {
-          query = JSON.parse(this.input('query'))
+          query = JSON.parse(ctx.request.input('query'))
         } catch (error) {
           throw InvalidArgumentException.invoke(error.message)
         }
       }
       query.where = query.where || {}
-      let includes = query.with || []
-      if (_.isString(includes)) {
-        includes = [{
-          relation: includes
-        }]
-      } else if (_.isArray(includes)) {
-        includes = includes.map(include => {
-          if (_.isString(include)) {
-            return {
-              relation: include
-            }
-          } else if (_.isObject(include)) {
-            if (!include.relation) {
-              throw InvalidArgumentException.invoke('`with` require relation property')
-            }
-            return include
-          } else {
-            throw InvalidArgumentException.invoke('`with` is not valid format')
-          }
-        })
-      } else if (_.isObject(includes)) {
-        if (!includes.relation) {
-          throw InvalidArgumentException.invoke('`with` require relation property')
-        }
-      } else {
-        throw InvalidArgumentException.invoke('`with` is not valid format')
-      }
-      query.with = includes
+      // let includes = query.with || []
+      // query.with = includes
       return _.pick(query, ['select', 'skip', 'limit', 'where', 'sort', 'with'])
     }
     // await next to pass the request to next middleware or controller
